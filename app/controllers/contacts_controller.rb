696903bf4be6588af356c5c5c3790df366babc2b
Narbe Voskanian <narbevoskanian@gmail.com>
2015-03-09 16:23:09 -0700

    Complete phase II

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,47 @@
+class ContactsController < ApplicationController
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      render json: @contact
+    else
+      render json: @contact.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.delete
+
+    render text: "contact destroyed"
+  end
+
+  def index
+    @contacts = Contact.all
+
+    render json: @contacts
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+
+    render json: @contact
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+
+    if @contact.update(contact_params)
+      render json: @contact
+    else
+      render json: @contact.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def contact_params
+      params[:contact].permit(:user_id, :email, :name)
+    end
+end
